58ce71ceb5c4155b3ae95dee72b2f0594294c86b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-13 11:57:04 +0600

    8_building the car card_part1_s1_t9

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -35,6 +35,26 @@
     <!-- CAR SIDE BAR -->
 
     <!-- CAR CARDS -->
+    <div class="w-full">
+      <!-- CAR CARD -->
+      <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+        <div class="flex h-full">
+          <img
+           src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg" 
+           alt=""
+           class="w-[300px] h-full"
+          >
+          <div class="p-4 flex flex-col">
+            <div>
+              <h1 class="text-2xl text-blue-700"> Volvo XC40</h1>
+              <p class="text-gray-700">Suscipit luctus incididunt, malesuada architecto, cupiditate.</p>
+            </div>
+            <h1 class="mt-auto text-xl">$39,555</h1>
+          </div>
+        </div>
+      </div>
+      <!-- CAR CARD -->
+    </div>
     <!-- CAR CARDS -->
    </div>
 
