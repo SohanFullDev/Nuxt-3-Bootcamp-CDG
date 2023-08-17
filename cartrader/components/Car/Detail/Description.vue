a36a802d6461bc12a55624209c42326b430e1311
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-17 11:25:13 +0600

    15.2_unique dynamic paths_part2_s2t15

---
+++ cartrader/components/Car/Detail/Description.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="mt-5">
+ <p class="mb-10">
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iusto tempora saepe ipsam facere ab similique pariatur natus non expedita deserunt numquam, aperiam labore dignissimos porro dolorem modi. Non, fugiat!
+ </p>
+
+</div>
+
+</template>
