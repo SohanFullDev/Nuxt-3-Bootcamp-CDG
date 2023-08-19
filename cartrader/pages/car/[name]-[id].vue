90324744f6c3d1662108402a2697a2b8e205c4d7
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-19 13:22:52 +0600

    21_defining a custom layout_s3

--- cartrader/pages/car/[name]-[id].vue
+++ cartrader/pages/car/[name]-[id].vue
@@ -2,7 +2,12 @@
  const route = useRoute()
  useHead({
     title: toTitleCase(route.params.name),
- })
+ });
+
+ definePageMeta({
+    layout: "custom"
+
+ });
 
  function toTitleCase(str){
     return str.replace(/\w\S*/g, function(txt){
@@ -14,26 +19,15 @@
 
 <template>
     <div>
-       
-        <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-
-
-<!-- CAR HERO -->
-
+             
 <CarDetailHero />
- <!-- CAR HERO -->
-
-
-<!-- CAR ATTRIBUTES -->
 
 <CarDetailAttributes />
-<!-- CAR ATTRIBUTES -->
-<!-- CAR DESCRIPTION -->
+
 <CarDetailDescription />
-<!-- CAR DESCRIPTION -->
-<!-- CAR CONTACT -->
+
 <CarDetailContact />
-<!-- CAR CONTACT -->
+
 </div>
-    </div>
+  
 </template>
--- cartrader/pages/city/[city]/car.vue
+++ cartrader/pages/city/[city]/car.vue
@@ -4,6 +4,11 @@
     title: `${route.params.make ? toTitleCase(route.params.make) : 'Cars'} in ${toTitleCase(route.params.city)}`,
  });
 
+ definePageMeta({
+    layout: "custom"
+
+ });
+
  function toTitleCase(str){
     return str.replace(/\w\S*/g, function(txt){
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
@@ -14,11 +19,7 @@
 
 <template>
     <div>
-      
-       
-   <!-- CARS PAGE -->
-   <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-    
+          
     <div class="mt-32 flex">
   
      <CarSideBar />
@@ -27,8 +28,7 @@
     </div>
  
     </div>
- 
-    </div>
+
 </template>
 <!-- 
     /city/houston/car
