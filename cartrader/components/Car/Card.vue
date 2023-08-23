1a7ba51588e7294c44d6dd91cb243e58f5f3748e
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-23 13:52:12 +0600

    33.2_the usestate composable_part2_s5t33

--- cartrader/components/Car/Card.vue
+++ cartrader/components/Car/Card.vue
@@ -6,17 +6,23 @@ import heartOutline from "@/assets/heartOutline.png";
 
   });
 
- 
+  const favored = useState(`favored-${props.car.id}`, () => {
+    return false
+
+   });
   
 </script>
 
 <template>
    <div class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-    @click="navigateTo(`/car/${car.name}-${car.id}`)"
+  
    >
-    <img class="absolute w-7 right-5 top-2 z-20" :src="heartOutline" alt="" >
+    <img class="absolute w-7 right-5 top-2 z-20" 
+    :src="favored ? heartFilled : heartOutline" 
+    alt="" 
+    @click="favored = !favored">
 
-         <div class="flex h-full">
+         <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
         
            <NuxtImg
             :src="car.url" 
