48cb4ddcffa03e72a57cf57bbf5302ab93d9c2c0
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-29 16:51:26 +0600

    37.3_adding the car price filter_part03#6*37

--- cartrader/components/Car/SideBar.vue
+++ cartrader/components/Car/SideBar.vue
@@ -59,6 +59,10 @@ const { makes } = useCars();
   navigateTo(`/city/${route.params.city}/car/${make}`)
 
  }
+ const onChangePrice = () => {
+  updateModal('price')
+
+ }
 
 </script>
 
@@ -101,7 +105,7 @@ const { makes } = useCars();
         <!-- PRICE START -->
        <div class="p-5 flex justify-between relative cursor-pointer border-b">
          <h3>Price</h3>
-         <h3 class="text-blue-400 capitalize">{{priceRangeText}}</h3>
+         <h3 class="text-blue-400 capitalize" @click="updateModal('price')">{{priceRangeText}}</h3>
          <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
           v-if="modal.price">
         <input
@@ -115,7 +119,7 @@ const { makes } = useCars();
          type="number" 
          placeholder="Max" 
          v-model="priceRange.max" /> 
-          <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
+          <button class="bg-blue-400 w-full mt-2 rounded text-white p-1" @click="onChangePrice">Apply</button>
          </div>
        </div>
        <!-- PRICE END -->
