ef115eb3e4f1c693d3ca8b0683099834d0c477b3
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-30 13:47:46 +0600

    39.5_assembling the pages_part6

--- cartrader/pages/profile/listings/create.vue
+++ cartrader/pages/profile/listings/create.vue
@@ -1,18 +1,40 @@
 <script setup>
     definePageMeta({
-        layout: "custom"
+        layout: "custom",
     });
 
-  const {makes} = useCars()  
+  const {makes} = useCars();
+
+  const info = useState("adInfo", () => {
+    return {
+        make: "",
+        model: "",
+        year: "",
+        miles: "",
+        price: "",
+        city: "",
+        seats: "",
+        description: "",
+        image: null,
+    }
+
+  });
+
+  const onChangeInput = (data, name) =>{
+    info.value[name] = data;
+
+  };
+
 
 </script>
 <template>
     <div>
         <div class="mt-24">
-            <h class="text-6xl">Creating a New Listing</h>
+            <h1 class="text-6xl">Creating a New Listing</h1>
         </div>
+        {{ info.make }}
         <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
-            <CarAdSelect title="Make *" :options="makes" />
+            <CarAdSelect title="Make *" :options="makes" name="make" @change-input="onChangeInput" />
         </div>
     </div>
 </template>
