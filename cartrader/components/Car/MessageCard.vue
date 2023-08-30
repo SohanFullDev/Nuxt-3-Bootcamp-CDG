7911c8689d92b7a4855d3f444fcb207c9c57819c
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-30 11:18:31 +0600

    39.3_assembling the pages_part4

---
+++ cartrader/components/Car/MessageCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="even:bg-gray-100 p-4">
+        <h1 class="font-fold text-2xl">Programmer Sohan</h1>
+        <p class="text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, nisi aspernatur. Debitis aperiam molestias magni, exercitationem repellat ducimus quibusdam recusandae modi atque possimus provident asperiores, voluptates quas aliquam! Deleniti, non?
+        </p>
+        <div class="flex items-center mt-4">
+            <p class="text-green-700 mr-10">
+                programmersohan@email.com
+            </p>
+            <p class="text-blue-500">  555-555-555  </p>
+        </div>
+    </div>
+</template>
---
+++ cartrader/pages/profile/listings/view/[id].vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="rounded shadow mt-20">
+        <CarMessageCard/>
+    </div>
+</template>
