1153100f15ec2ac73269cbd86933a82303a2a43f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-30 12:20:52 +0600

    39.4_assembling the pages_part5

---
+++ cartrader/pages/profile/listings/create.vue
@@ -0,0 +1,18 @@
+<script setup>
+    definePageMeta({
+        layout: "custom"
+    });
+
+  const {makes} = useCars()  
+
+</script>
+<template>
+    <div>
+        <div class="mt-24">
+            <h class="text-6xl">Creating a New Listing</h>
+        </div>
+        <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
+            <CarAdSelect title="Make *" :options="makes" />
+        </div>
+    </div>
+</template>
