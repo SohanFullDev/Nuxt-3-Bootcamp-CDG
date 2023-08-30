a4acc972d5cfcc09dd1d487c74af032e6547e5c1
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-30 10:05:54 +0600

    39.1_assembling the pages_part2#7

---
+++ cartrader/components/Car/ListingCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const props = defineProps({
+        listing: Object,
+    });
+
+</script>
+
+<template>
+    <div class="shadow rounded overflo-hidden flex justify-between mb-4">
+        <div class="flex">
+            <img src="" alt="" class="w-80 mr-3 h-44">
+            <div class="p-3">
+                <h1 class="text-2xl"></h1>
+                <p class="text-blue-400"></p>
+            </div>
+        </div>
+        <div class="p-3 flex">
+            <NuxtLink to="/profile/listing/view/4">View</NuxtLink>
+            <p class="text-red-400 cursor-pointer">Delete</p>
+        </div>
+    </div>
+</template>
--- cartrader/pages/profile/listings/index.vue
+++ cartrader/pages/profile/listings/index.vue
@@ -11,5 +11,9 @@
             <h1 class="text-6xl">My Listings</h1>
     <NuxtLink to="/profile/listings/create" class="w-9 h-9 bg-blue-400 flex justify-center items-center rounded-full text-white font-bold cursor-pointer">+</NuxtLink>
         </div>
+        <div class="shadow rounded p-3 mt-5">
+            
+
+        </div>
     </div>
 </template>
